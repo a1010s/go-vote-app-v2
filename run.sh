8fc9ba7d2e2cac227d8441cf0f997104b34b367a
a1010s <andrei-stegaru@gmx.de>
2023-11-10 18:50:16 +0100

    fast-auto commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./myapp &
+sleep 30
+echo "Stopping the application on the server..."
+pkill -f myapp # Kill the process group using the saved PID
+rm /root/go-vote-app-v2/myapp
+rm -rf badger-db
