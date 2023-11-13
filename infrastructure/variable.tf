1e81170ac149264338845c615db7fab738d653f0
a1010s <andrei-stegaru@gmx.de>
2023-11-13 10:39:33 +0100

    fast-auto commit

--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -8,8 +8,3 @@ variable "id_name" {
   default = "go-vote-app"
   description = "name of the servers"
 }
-
-variable "ssh-pub-key" {
-  type = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCVbBA/wv5cHSvviuWwb8RgMxZKXIqLmBVLUVqKd58Ln/SnMMmtjUhDneXuiK1AEuYaUlVieRBB6Vk2a67PvI6xZE4ZGwJUcnB1MCA5YiZ18moJqaJRBwVYGjPXDfmNHBmVSkjTFuhr/oTNOhW/S7XoPVMs92A7sSQ43UTQ5iiX7vQKPZUCVqdfmO2LDJUYOP7F3ixS0ZUTsk9VPLdeT10GBXuRwZ5dfjeNbrL+A2Y9gveG/jACSKG/gn5RGT3U4WIIzOLhEXJAS8njMEIW7WN8wxnhYHl5i/nTH+iRNzNFcklxcRkHerObpCdkWXtAYRu9VmUHsS441ira1KdgONBt astegaru@lb02117"
-}
